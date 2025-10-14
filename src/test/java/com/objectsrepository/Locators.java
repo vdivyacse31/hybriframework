8a595715d5e170b4c80c1e7e278b162720ce6b8c
Divya Vagalaboina <divya.vagalaboina.external@atos.net>
2025-10-13 22:01:58 -0400

    created classes

---
+++ src/test/java/com/objectsrepository/Locators.java
@@ -0,0 +1,5 @@
+package com.objectsrepository;
+
+public class Locators {
+
+}
---
+++ src/test/java/com/testscenarios/MyFirstProgram.java
@@ -0,0 +1,10 @@
+package com.testscenarios;
+
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
